0c0dae51c3c7ce2d169c1d2fad4f69991abd3ca8
cprachaseree <jasoncprach@gmail.com>
2020-01-20 23:29:02 +0800

    Added header files and makefile

---
+++ myftp.h
@@ -0,0 +1,9 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
