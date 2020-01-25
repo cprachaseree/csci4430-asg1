5e3c2905c8f3c6bd717e724400498438f4662b4e
cprachaseree <1155102620@link.cuhk.edu.hk>
2020-01-25 17:23:26 +0800

    added arguments error checking

--- myftp.c
+++ myftp.c
@@ -1,1 +1,22 @@
 #include "myftp.h"
+
+void print_arg_error(char *role) {
+	printf("Invalid Arguments\n");
+	if (strcmp(role, "server") == 0) {
+		printf("Usage ./server <PORT_NUMBER>\n");
+	} else if (strcmp(role, "client") == 0) {
+		printf("Usage: ./client <SERVER_IP_ADDRESS> <SERVER_PORT_NUMBER> <list|get|put> <FILE_NAME>\n");
+	}
+	printf("Program terminated.\n");
+	exit(0);
+}
+
+int port_num_to_int(char *port_num_string, char *role) {
+	int port_num_int;
+  	char *end_ptr;
+  	port_num_int = (int) strtol(port_num_string, &end_ptr, 0);
+  	if (*end_ptr != '\0') {
+    	print_arg_error(role);
+  	}
+  	return port_num_int;
+}
--- myftp.h
+++ myftp.h
@@ -7,3 +7,6 @@
 # include <sys/types.h>
 # include <netinet/in.h>
 # include <arpa/inet.h>
+
+void print_arg_error(char *role);
+int port_num_to_int(char *port_num_string, char *role);
