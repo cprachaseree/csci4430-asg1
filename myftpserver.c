022ea6a0fe1aa679cebd141a73a81540738461ef
cprachaseree <1155102620@link.cuhk.edu.hk>
2020-01-26 13:59:12 +0800

    checked argument myftpserver

--- myftpserver.c
+++ myftpserver.c
@@ -1,22 +1,15 @@
 #include "myftp.h"
 
+void check_arg(int argc);
 int check_port_num(int arg_num, char *port_number_string);
 
 int main(int argc, char *argv[]) {
-  int PORT_NUMBER = check_port_num(argc, argv[1]);
+  check_arg(argc);
+  int PORT_NUMBER = port_num_to_int(argv[1], "server");
 }
 
-int check_port_num(int arg_num, char *port_number_string) {
-  int port_number_int;
-  char *end_ptr;
-  if (arg_num != 2) {
-    printf("Invalid arguments. Usage ./server <PORT_NUMBER>\n");
-    exit(0);
+void check_arg(int argc) {
+  if (argc != 2) {
+    print_arg_error("server");
   }
-  port_number_int = (int) strtol(port_number_string, &end_ptr, 0);
-  if (*end_ptr != '\0') {
-    printf("Invalid argument. Usage ./server <PORT_NUMBER>\n");
-    exit(0);
-  }
-  return port_number_int;
 }
