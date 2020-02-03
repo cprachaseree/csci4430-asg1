7da87574fd069f51280bae56697299f88ab33cb0
pohchaichon <poh_chaichon@hotmail.com>
2020-02-03 11:15:45 +0700

    tab size from 2 to 4

--- myftpserver.c
+++ myftpserver.c
@@ -4,12 +4,12 @@ void check_arg(int argc);
 int check_port_num(int arg_num, char *port_number_string);
 
 int main(int argc, char *argv[]) {
-  check_arg(argc);
-  int PORT_NUMBER = port_num_to_int(argv[1], "server");
+    check_arg(argc);
+    int PORT_NUMBER = port_num_to_int(argv[1], "server");
 }
 
 void check_arg(int argc) {
-  if (argc != 2) {
+    if (argc != 2) {
     print_arg_error("server");
   }
 }
