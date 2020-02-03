ff941e56940bbb8bbcb79816ac565d83bdcb0a3e
WONGKHAM Chaichon <wongkha8@linux9.cse.cuhk.edu.hk>
2020-02-03 22:20:42 +0800

    server open socket

--- myftpserver.c
+++ myftpserver.c
@@ -6,6 +6,12 @@ int check_port_num(int arg_num, char *port_number_string);
 int main(int argc, char *argv[]) {
     check_arg(argc);
     int PORT_NUMBER = port_num_to_int(argv[1], "server");
+	int sd = socket(AF_INET, SOCK_STREAM, 0)
+	if (fd == -1) {
+	// fail to open socket
+	return -1
+	}
+	
 }
 
 void check_arg(int argc) {
