1792b59e7197e5bef33219ad5a6529a3132f74a4
pohchaichon <poh_chaichon@hotmail.com>
2020-02-04 12:14:21 +0700

    test server socket + thread

--- myftpserver.c
+++ myftpserver.c
@@ -1,21 +1,66 @@
 #include "myftp.h"
+#include<pthread.h>
 
 void check_arg(int argc);
 int check_port_num(int arg_num, char *port_number_string);
+void* connection(int client_sd);
 
 int main(int argc, char *argv[]) {
     check_arg(argc);
     int PORT_NUMBER = port_num_to_int(argv[1], "server");
-	int sd = socket(AF_INET, SOCK_STREAM, 0)
-	if (fd == -1) {
-	// fail to open socket
-	return -1
+    // open socket
+	int sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sd < 0) {
+        prinf("open socket failed: %s (Errno: %d)\n", strerror(errno), errno);
+	    return -1;
 	}
-	
+    // bind port to socket
+	struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(PORT_NUMBER);
+    if(bind(sd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+		printf("bind failed: %s (Errno: %d)\n", strerror(errno), errno);
+		return -1;
+	}
+    if(listen(sd, 3) < 0) {
+		printf("listen failed: %s (Errno: %d)\n",strerror(errno), errno);
+		return -1;
+	}
+    pthread_t threads[10];
+    int thread_count = 0;
+    while(1) {
+        // keep accepting new connections
+        struct sockaddr_in client_addr;
+        int addr_len = sizeof(client_addr);
+        int client_sd;
+	    if((client_sd = accept(sd, (struct sockaddr *) &client_addr, &addr_len))<0){
+		    printf("accept failed: %s (Errno: %d)\n", strerror(errno), errno);
+            return -1;
+	    }
+        // creating thread for transmission stage to support concurrent clients
+        pthread_create(&threads[thread_count], NULL, &connection, client_sd);
+        thread_count++;
+    }
 }
 
 void check_arg(int argc) {
     if (argc != 2) {
     print_arg_error("server");
   }
+}
+
+void* connection(int client_sd) {
+    while(1) {
+    char buff[100];
+    int len;
+    if((len=recv(client_sd,buff,sizeof(buff),0))<0){
+        printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+        exit(0);
+    }
+    buff[len]='\0';
+    printf("RECEIVED INFO: ");
+    if(strlen(buff)!=0)printf("%s\n",buff);
+    }
 }
