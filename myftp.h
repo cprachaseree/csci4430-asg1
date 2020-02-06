13ac1bd55c981d94ce9d0730b257aa3d7cbdc7c5
cprachaseree <1155102620@link.cuhk.edu.hk>
2020-02-06 17:43:39 +0800

    added struct message_s

--- myftp.h
+++ myftp.h
@@ -10,3 +10,10 @@
 
 void print_arg_error(char *role);
 int port_num_to_int(char *port_num_string, char *role);
+
+struct message_s {
+	unsigned char protocol[5];   /*protocol string (5 bytes)*/
+	unsigned char type;          /*type (1 byte)*/
+	unsigned int length;         /*length (header + payload) (4 bytes)*/
+} __attribute__ ((packed));
+
