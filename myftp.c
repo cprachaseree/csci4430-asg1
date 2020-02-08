61304a037afd2111bb16aaa96529a1b4cbaeebfc
cprachaseree <1155102620@link.cuhk.edu.hk>
2020-02-08 21:36:45 +0800

    functions for send and receive files

--- myftp.c
+++ myftp.c
@@ -1,4 +1,5 @@
 #include "myftp.h"
+#define BUFFER_SIZE 2
 
 void print_arg_error(char *role) {
 	printf("Invalid Arguments\n");
@@ -19,4 +20,113 @@ int port_num_to_int(char *port_num_string, char *role) {
     	print_arg_error(role);
   	}
   	return port_num_int;
+}
+
+void send_file_header(int destination_sd, int file_size) {
+	int len;
+	// send file data header
+	struct message_s file_data;
+    memset(&file_data, 0, sizeof(struct message_s));
+    strcpy(file_data.protocol, "myftp");
+    file_data.type = 0xFF;
+    file_data.length = file_size + sizeof(struct message_s);
+    if ((len = send(destination_sd, &file_data, sizeof(struct message_s), 0)) < 0) {
+        printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
+        exit(0);
+    }
+}
+
+// used in client when get and in server when put
+// need to loop
+void send_file(int destination_sd, int file_size, char *file_name) {
+	int len;
+    // send file payload
+    FILE *fp;
+    fp = fopen(file_name, "r");
+    char buffer[BUFFER_SIZE];
+    for (int i = 0; i < file_size / BUFFER_SIZE; i++) {
+    	fread(buffer, BUFFER_SIZE, 1, fp);
+    	if ((len = send(destination_sd, buffer, BUFFER_SIZE, 0)) < 0) {
+        	printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
+        	exit(0);
+    	}
+    	printf("Sent file contents:\n");
+        printf("%s\n", buffer);
+    }
+    int last_contents_size = file_size % BUFFER_SIZE;
+    if (last_contents_size != 0) {
+    	char *last_contents = (char *) calloc (last_contents_size, sizeof(char));
+	    fread(last_contents, last_contents_size, 1, fp);
+	    if ((len = send(destination_sd, last_contents, last_contents_size, 0)) < 0) {
+	        printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
+	        exit(0);
+	    }
+	    printf("Sent file contents last:\n");
+	    printf("%s\n", last_contents);
+    }
+    fclose(fp);
+    printf("Sent file done.\n");
+}
+
+// used in server when get and in client when put
+void receive_file(int source_sd, int file_size, char *file_name) {
+	int len;
+	// receive file payload
+	FILE *fp;
+    fp = fopen(file_name, "w");
+    char buffer[BUFFER_SIZE];
+    for (int i = 0; i < file_size / BUFFER_SIZE; i++) {
+    	if ((len = recv(source_sd, buffer, BUFFER_SIZE, 0)) < 0) {
+			printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+	        exit(0);
+		}
+    	printf("Received file contents:\n");
+		printf("%s\n", buffer);
+		fprintf(fp, "%s", buffer);
+    }
+    int last_contents_size = file_size % BUFFER_SIZE;
+    if (last_contents_size != 0) {
+    	char *last_contents = (char *) calloc (last_contents_size, sizeof(char));
+    	if ((len = recv(source_sd, last_contents, last_contents_size, 0)) < 0) {
+			printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+        	exit(0);
+		}
+    	printf("Received file contents last:\n");
+    	printf("%s\n", last_contents);
+		fprintf(fp, "%s", last_contents);
+    }
+    fclose(fp);
+    printf("Received file done.\n");
+}
+
+// returns the file length after checking file data header
+int check_file_data_header(int source_sd) {
+	int len;
+	// receive file header
+	struct message_s file_data;
+	memset(&file_data, 0, sizeof(struct message_s));
+	if ((len = recv(source_sd, &file_data, sizeof(struct message_s), 0)) < 0) {
+		printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+	    exit(0);
+	}
+	// check if file header protocol is correct
+	if (strncmp(file_data.protocol, "myftp", 5) != 0 || file_data.type != 0xFF) {
+		printf("Invalid header type or protocol.\n");
+		exit(0);
+	}
+
+	return file_data.length;
+}
+
+// used in client when put and in server when get
+int get_file_size(char *file_name) {
+	FILE *fp;
+	if ((fp = fopen(file_name, "r")) == NULL) {
+		return -1;
+    }
+	fseek(fp, 0, SEEK_END);
+    int file_size = ftell(fp);
+    rewind(fp);
+    fclose(fp);
+    return file_size;
 }
--- myftp.h
+++ myftp.h
@@ -10,6 +10,11 @@
 
 void print_arg_error(char *role);
 int port_num_to_int(char *port_num_string, char *role);
+void send_file_header(int destination_sd, int file_size);
+void send_file(int destination_sd, int file_size, char *file_name);
+void receive_file(int source_sd, int file_size, char *file_name);
+int check_file_data_header(int source_sd);
+int get_file_size(char *file_name);
 
 struct message_s {
 	unsigned char protocol[5];   /*protocol string (5 bytes)*/
